32771429282516ea74a50a3964fe49b8580b3ddf
Deewakar2002Jha <deewakarjha0541@gmail.com>
2026-03-23 02:59:41 +0530

    version 5

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
+const express = require('express');
 const app = require('../backend/server');
-module.exports = app;
+
+// This tells Express that our Vercel function is mounted at /api
+// so it can correctly route requests like /api/notes to /notes
+const mainApp = express();
+mainApp.use('/api', app);
+
+module.exports = mainApp;
